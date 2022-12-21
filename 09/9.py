e36c16f01344094394547c2b4e834a4cce0ff139
Аля <alaisinbaeva00@gmail.com>
2022-12-21 20:57:32 +0500

    Complete 09 lesson

---
+++ 09/9.py
@@ -0,0 +1,8 @@
+capital = list()
+for i in range(int(input())):
+    capital.append(int(input()))
+for elem in capital:
+    print(elem)
+print()
+for i in range(len(capital)):
+    print(capital[i] * 3)
